feb6ec6b3443f87e24932534c1d27c2c3d2eaf1e
ruhulamin1398 <ruhul.ok@gmail.com>
2023-11-05 09:49:27 +0600

    contract deployed

--- contracts/MyNft.sol
+++ contracts/MyNft.sol
@@ -1,22 +1,27 @@
-import "@openzeppelin/contracts/utils/Counters.sol"; 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";   
 
 contract MyNFT is ERC721URIStorage,Ownable{
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    uint256 private _nextTokenId;
 
-    constructor() ERC721("AK Coins","AK"){
+    constructor() ERC721("GameItem", "ITM") Ownable(msg.sender) {
 
     }
 
-    function mintNFT(address recipient,string memory tokenURI) public onlyOwner returns (unit256){
-        _tokenIds.increment();
+        function mintNFT(address recipient, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256){
+        
 
-        unt256 newItemId= _tokenIds.current();
+        uint256 newItemId= _nextTokenId++;
         _mint(recipient , newItemId);
-        _setTokenURI(newItemId,tokenURI)
+        _setTokenURI(newItemId,tokenURI);
         return newItemId;
     }
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+ 
+
+
+require('@nomiclabs/hardhat-waffle');
+const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: "0.8.19",
-};
+  solidity: '0.8.20',
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/VJYxtBrrBhmgTayoeT4pr8830MY69Qfp',
+      accounts: ['20cec08faea52eb9f022ab7c0f79224d6fe8c1594e3126d6367a3c669080f33a'],
+    },
+  },
+}
